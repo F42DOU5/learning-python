ffcd18c49864488bfc1b838a4d7aee1bdb190939
Fardousks97 <www.fardous@outlook.com>
2021-05-18 21:10:38 +0600

    feat:block of codes

---
+++ flowcontrol/blockofcode.py
@@ -0,0 +1,10 @@
+name= input()
+if name == "Kuddus":
+    print("Acccess Granted")
+else:
+    print("Fake User")
+password= input()
+if password == "2030":
+    print("Access Granted")
+else:
+    print("Fake User")
