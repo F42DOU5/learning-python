396ff44ade3500883451b43cafdaf977ab069de2
Fardousks97 <www.fardous@outlook.com>
2021-06-15 12:52:53 +0600

    feat: make correction of of logic

---
+++ flowcontrol/ifelse.py
@@ -0,0 +1,10 @@
+name= input()
+age = input()
+if name == "Alice":
+    print('Hi Alice')
+elif age < 12:
+    print('you are not Alice,kid')
+elif age > 2000:
+    print('You are godzila')
+elif age > 100:
+    print('you are a grandma')
