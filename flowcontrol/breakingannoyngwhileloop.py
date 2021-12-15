3c7fa776a6c0f099a44a6ddd3819cf07ae568d18
Fardous Khan <iamhasanalfardous@gmail.com>
2021-12-16 01:08:08 +0600

    Delete flowcontrol directory

--- flowcontrol/breakingannoyngwhileloop.py
+++
@@ -1,7 +0,0 @@
-name = ''
-while name != 'noname':
-     print("please type your name")
-     name = input()
-print("Thanks")
-    
-    
--- flowcontrol/whileloop.py
+++
@@ -1,6 +0,0 @@
-spam = 0
-while spam < 10:
-    print ("correct")
-    spam += 1
-    
-    
